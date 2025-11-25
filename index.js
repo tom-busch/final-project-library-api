84ee1f5b988d87e73770f3c7e3ca47a0d6d5265d
Tom Busch <tbusch1@charlotte.edu>
2025-11-24 22:34:29 -0500

    Fix render directory issue

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-// Detect environment and load server accordingly
-const isRender = process.env.RENDER === 'true';
-const serverModule = isRender ? './server.js' : './src/server.js';
-
-import(serverModule).catch(err => {
+import('./src/server.js').catch(err => {
   console.error('Failed to start server:', err);
   process.exit(1);
 });

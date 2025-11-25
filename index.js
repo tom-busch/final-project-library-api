cd4dc3e570515b5c0b777aa17d2a27b11467686b
Tom Busch <tbusch1@charlotte.edu>
2025-11-24 22:28:58 -0500

    created debug script

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import('./src/server.js').catch(err => {
+// Detect environment and load server accordingly
+const isRender = process.env.RENDER === 'true';
+const serverModule = isRender ? './server.js' : './src/server.js';
+
+import(serverModule).catch(err => {
   console.error('Failed to start server:', err);
   process.exit(1);
 });
---
+++ server.js
@@ -0,0 +1,3 @@
+// This file is used when RENDER=true
+// It re-exports the actual server from src/server.js
+export { default } from './src/server.js';
